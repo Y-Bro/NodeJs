b91686338139836becebf28e5f398048cca6c51a
YerrumsettyBharath <yerrumsetty.bharath@gmail.com>
2021-05-08 19:31:09 +0530

    post request made

--- expressApp/controller/user.js
+++ expressApp/controller/user.js
@@ -1,6 +1,20 @@
-let users = [];
+
+let users = [
+    {
+        "Name" : "Bharath",
+        "Age" : "20",
+        "id" : 1
+    }
+];
 
 export const getUsers = (req,res) =>{
     res.status(200).send(users);
     console.log("INISDE getUsers");
+}
+
+export const postUsers = (req,res) =>{
+    let newUser = req.body;
+    newUser.id = users[users.length-1].id + 1;
+    users.push(newUser);
+    res.sendStatus(201);
 }
--- expressApp/router/user.js
+++ expressApp/router/user.js
@@ -1,10 +1,13 @@
 import express from "express";
 import {getUsers} from "../controller/user.js";
+import {postUsers} from "../controller/user.js";
 
 
 const router = express.Router();
 
 router.get("/",getUsers);
+
+router.post("/",postUsers);
 
 
 export default router;
