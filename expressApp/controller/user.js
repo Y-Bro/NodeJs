3d8655f25bb78fa38212524a3e7469b7928752d4
YerrumsettyBharath <yerrumsetty.bharath@gmail.com>
2021-05-08 23:07:20 +0530

    8th may

--- expressApp/controller/user.js
+++ expressApp/controller/user.js
@@ -1,20 +1,54 @@
-
+import {v4 as uuid} from 'uuid';
 let users = [
-    {
-        "Name" : "Bharath",
-        "Age" : "20",
-        "id" : 1
-    }
+   
 ];
 
 export const getUsers = (req,res) =>{
     res.status(200).send(users);
-    console.log("INISDE getUsers");
 }
 
 export const postUsers = (req,res) =>{
     let newUser = req.body;
-    newUser.id = users[users.length-1].id + 1;
-    users.push(newUser);
+    let id = uuid();
+    users.push({...newUser,id : id});
+    res.status(201).send(id);
+}
+
+export const getUserById = (req,res) =>{
+    let id = req.params.id;
+    console.log(id);
+    const user = users.find(user =>{
+        return user.id === id;
+    })
+    res.status(200).send(user);
+}
+
+export const deleteUserById = (req,res) => {
+    users = users.filter(user =>{
+       return user.id!==req.params.id;
+    })
+    res.sendStatus(201);
+}
+
+export const patchNameById = (req,res) =>{
+    const user = users.find(x =>{
+        return x.id === req.params.id;
+    })
+    user.Name = req.body.name;
+    res.sendStatus(201);
+}
+
+export const updateById = (req,res) =>{
+    users = users.map(e=>{
+        if(e.id === req.params.id)
+        {
+            e.Name = req.body.Name;
+            e.Age = req.body.age;
+            e.Engineer = req.body.Engineer;
+            e.hobbies = req.body.hobbies;
+        }
+        return e;
+    })
+
     res.sendStatus(201);
 }
--- expressApp/router/user.js
+++ expressApp/router/user.js
@@ -1,13 +1,14 @@
 import express from "express";
-import {getUsers} from "../controller/user.js";
-import {postUsers} from "../controller/user.js";
-
+import {getUsers,postUsers,getUserById,deleteUserById,patchNameById,updateById} from "../controller/user.js";
 
 const router = express.Router();
 
 router.get("/",getUsers);
-
+router.get("/:id",getUserById);
 router.post("/",postUsers);
+router.delete("/:id",deleteUserById);
+router.patch("/:id",patchNameById);
+router.put("/:id",updateById);
 
 
 export default router;
